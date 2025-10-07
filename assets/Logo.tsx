c3592b868076acded5a2f0f7d0e3393b4e05ed0f
carolRag <carolgarradoalegria@gmail.com>
2025-10-07 06:00:33 -0400

    Logo mal centreado :(

--- assets/Logo.tsx
+++ assets/Logo.tsx
@@ -1,18 +1,26 @@
 import React from "react";
-import { Image, Text, View } from "react-native";
+import { Image, View } from "react-native";
 
 export default function Logo() {
   return (
-    <View className="items-center justify-center">
+    <View
+      style={{
+        alignItems: "flex-start",  // Alinea el logo a la izquierda
+        justifyContent: "flex-start",
+        marginLeft: 20,            // Ajusta margen izquierdo
+        marginTop: 30,             // Ajusta margen superior
+      }}
+    >
       <Image
         source={{
           uri: "https://i.ibb.co/0jGXFZBn/Chat-GPT-Image-7-oct-2025-05-09-19.png",
         }}
-        style={{ width: 120, height: 120, resizeMode: "contain" }}
+        style={{
+          width: 250,             // Más grande
+          height: 250,
+          resizeMode: "contain",
+        }}
       />
-      <Text className="mt-2 text-lg font-semibold text-gray-700">
-        Mi Empresa
-      </Text>
     </View>
   );
 }
